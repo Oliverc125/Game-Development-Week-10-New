0ae56d443f36f68e16c063e1ee3562998e237bfc
Oliverc125 <olivercutts760@gmail.com>
2024-12-19 14:01:38 +0000

    Namespace Refactoring Complete

--- Game Dev 3 - Week 10/Assets/_Scripts/Player/Player_Collision.cs
+++ Game Dev 3 - Week 10/Assets/_Scripts/Player/Player_Collision.cs
@@ -1,63 +1,68 @@
-﻿using System.Collections;
+﻿using GameDevWithMarco.Managers;
+using GameDevWithMarco.Vfx;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Collision : MonoBehaviour
+namespace GameDevWithMarco.Player
 {
-    //References
-    private VfxManager vfx;
-    private Ripple ripple;
-    public  UIManager ui;
+    public class Player_Collision : MonoBehaviour
+    {
+        //References
+        private VfxManager vfx;
+        private Ripple ripple;
+        public  UIManager ui;
     
-    //Variables
-    public bool greenCollected = false;
+        //Variables
+        public bool greenCollected = false;
     
-    void Start()
-    {
-        Initialisation();
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        ExecuteLogicBasedOnWhatWeHaveCollidedWith(collision);
-        Destroy(collision.gameObject);
-    }
-
-    private void ExecuteLogicBasedOnWhatWeHaveCollidedWith(Collider2D collision)
-    {
-        switch (collision.gameObject.tag)
+        void Start()
         {
-            case "GoodBox":
-                GameManager.Instance.GreenPackLogic();
-                vfx.GoodPickupParticles();
-                vfx.AddPointsPromptMethod();
-                ripple.RippleReaction();
-                AudioManager.Instance.GoodPickupSound();
-                greenCollected = true;
-                break;
-            case "BadBox":                
-                GameManager.Instance.RedPackLogic();
-                vfx.CamShake();
-                vfx.BadPickupParticles();
-                vfx.SubtractPointsPromptMethod();
-                ui.MinusOneLifeFeedback();
-                AudioManager.Instance.BadPickupSound();
-                break;
-            case "LifeBox":
-                GameManager.Instance.lives++;
-                ripple.RippleReaction();
-                ui.PlusOneLifeFeedback();
-                AudioManager.Instance.LifePickupSound();
-                break;
-            default:
-                break;
+            Initialisation();
         }
-    }
 
-    private void Initialisation()
-    {
-        vfx             = FindObjectOfType<VfxManager>();
-        ripple          = FindObjectOfType<Ripple>();
-        ui              = FindObjectOfType<UIManager>();
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            ExecuteLogicBasedOnWhatWeHaveCollidedWith(collision);
+            Destroy(collision.gameObject);
+        }
+
+        private void ExecuteLogicBasedOnWhatWeHaveCollidedWith(Collider2D collision)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "GoodBox":
+                    GameManager.Instance.GreenPackLogic();
+                    vfx.GoodPickupParticles();
+                    vfx.AddPointsPromptMethod();
+                    ripple.RippleReaction();
+                    AudioManager.Instance.GoodPickupSound();
+                    greenCollected = true;
+                    break;
+                case "BadBox":                
+                    GameManager.Instance.RedPackLogic();
+                    vfx.CamShake();
+                    vfx.BadPickupParticles();
+                    vfx.SubtractPointsPromptMethod();
+                    ui.MinusOneLifeFeedback();
+                    AudioManager.Instance.BadPickupSound();
+                    break;
+                case "LifeBox":
+                    GameManager.Instance.lives++;
+                    ripple.RippleReaction();
+                    ui.PlusOneLifeFeedback();
+                    AudioManager.Instance.LifePickupSound();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Initialisation()
+        {
+            vfx             = FindObjectOfType<VfxManager>();
+            ripple          = FindObjectOfType<Ripple>();
+            ui              = FindObjectOfType<UIManager>();
+        }
     }
 }
